e409774b6a9a66a705016b4c74b9613557c2c0d8
gregoireFessard <gregoire.fessard@gmail.com>
2019-05-18 00:03:40 +0200

    added comment

--- imageFunction.cpp
+++ imageFunction.cpp
@@ -1,5 +1,16 @@
 #include "imageFunction.h"
 
+// to get the image displayed :
+//	Mat img;
+//	frame.getImage(img);
+
+// to update the view of the image:
+// frame.updateBackground();
+
+// to "save" the modification of the image in the program, so that the next getImage will send the modified image:
+// frame.modifyImage(img);
+
+
 void bright(Frame& frame)
 {
 	frame.updateImage();
